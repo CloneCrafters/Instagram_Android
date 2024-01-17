e5cf6264897072d5996e75ba5300621d5d0fb866
KJM <kjm99d@naver.com>
2024-01-17 23:42:24 +0900

    💄 상단 메뉴 UI 작업

--- app/src/main/java/com/clonecrafters/instagram/MainActivity.java
+++ app/src/main/java/com/clonecrafters/instagram/MainActivity.java
@@ -1,14 +1,32 @@
 package com.clonecrafters.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private FragmentManager fragmentManager;
+    private TopLayoutFragment topLayoutFragment;
+    private FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CreateFramgment_TopLayout();
+    }
+
+    private void CreateFramgment_TopLayout()
+    {
+        fragmentManager = getSupportFragmentManager();
+
+        topLayoutFragment = new TopLayoutFragment();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, topLayoutFragment).commitAllowingStateLoss();
     }
 }
